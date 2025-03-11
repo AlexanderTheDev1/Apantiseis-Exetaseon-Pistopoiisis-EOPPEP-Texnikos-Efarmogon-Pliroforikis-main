963ef0d46796cd6e1669b8932c32cfe2b64b6f1b
AlexanderTheDev <134628095+AlexanderTheDev1@users.noreply.github.com>
2025-03-11 12:06:14 +0200

    better styles and some js

---
+++ content/js/script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.selected').forEach(function(element) {
+    
+    element.addEventListener('click', function() {
+      
+        this.classList.toggle('active');
+    });
+});
+
