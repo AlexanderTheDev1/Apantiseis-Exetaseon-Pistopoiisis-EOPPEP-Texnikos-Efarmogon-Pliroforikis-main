e0553f7c1e39724f042ce19bad93153486d1e77d
AlexanderTheDev <134628095+AlexanderTheDev1@users.noreply.github.com>
2025-03-11 15:34:09 +0200

    update script.js

--- content/js/script.js
+++ content/js/script.js
@@ -3,5 +3,17 @@ document.querySelectorAll('.selected').forEach(function(element) {
     element.addEventListener('click', function() {
       
         this.classList.toggle('active');
+    }); 
+});
+
+
+
+document.querySelectorAll('.active').forEach(function(element) {
+if ('Q-1 active' && 'A-1 active') {
+    element.addEventListener('click', function() {
+        this.classList.toggle('correct');
     });
+} element.addEventListener('click', function() {
+    this.classList.toggle('wrong');
+});
 });
