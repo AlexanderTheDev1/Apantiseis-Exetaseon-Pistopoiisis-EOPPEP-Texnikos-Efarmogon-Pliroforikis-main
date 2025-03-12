d5a164f524e2eabc53d15bc66083f1231ec4e7b0
AlexanderTheDev <134628095+AlexanderTheDev1@users.noreply.github.com>
2025-03-12 10:09:32 +0200

    removed unnecessary code

--- content/js/script.js
+++ content/js/script.js
@@ -1,19 +1,7 @@
-document.querySelectorAll('.selected').forEach(function(element) {
+document.querySelectorAll('.question, .answer').forEach(function(element) {
     
     element.addEventListener('click', function() {
       
         this.classList.toggle('active');
     }); 
-});
-
-
-
-document.querySelectorAll('.active').forEach(function(element) {
-if ('Q-1 active' && 'A-1 active') {
-    element.addEventListener('click', function() {
-        this.classList.toggle('correct');
-    });
-} element.addEventListener('click', function() {
-    this.classList.toggle('wrong');
-});
 });
